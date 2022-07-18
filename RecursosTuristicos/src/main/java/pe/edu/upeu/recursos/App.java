2d466c84480ebcc22f0a19decb52a67bc007d591
crhistianchuqui02 <crhistian.chuquitarqui@upeu.edu.pe>
2022-07-18 17:44:25 -0500

    proyectochuqui

---
+++ RecursosTuristicos/src/main/java/pe/edu/upeu/recursos/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.recursos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new MenuOpciones().login();
+    }
+}
